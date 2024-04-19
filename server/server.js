9908d067f5d1387570e5ff3f43aa231efe1a37ec
Khouloud Karoui <khoolood.loolood@gmail.com>
2024-04-19 22:19:40 +0200

    add backend

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const Todo = require("./models/Todo");
-app.use(cors());
+app.use(cors({ origin: "https://mern-todo-a-1.onrender.com" }));
 app.use(express.json());
 
 //mongoose.connect("MONGO_URI");
